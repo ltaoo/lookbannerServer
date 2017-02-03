be42f7d0cb988502990fdfe2dc489eedfaee150f
ltaoo <litaowork@aliyun.com>
2017-02-03 23:00:02 +0800

    【优化】jd采集代码并增加测试用例

--- collect/rules/jd.js
+++ collect/rules/jd.js
@@ -1,9 +1,7 @@
-var fs = require('fs')
 // third part
-var request = require('request')
-var cheerio = require('cheerio')
-var _ = require('lodash')
-var co = require('co');
+import request from 'request'
+import cheerio from 'cheerio'
+import _ from 'lodash'
 
 function req(url) {
   return new Promise((resolve, reject)=> {
@@ -15,42 +13,40 @@ function req(url) {
 }
 
 global.jsonpCallbackFocus = function(res) {
-  return res;
+  return res
 }
 
-var web = {
+const web = {
     url: 'https://www.jd.com',
-    rule: function(body, cb) {
-      return new Promise((resolve, reject) => {
-        let $ = cheerio.load(body);
+    rule(body) {
+      return new Promise(async function (resolve, reject) {
+        const $ = cheerio.load(body)
         // 从 html 中提取
         // 首先拿到前四个
-        let first = $('body')['0'].children[0].next.children[0].data;
+        let first = $('body')['0'].children[0].next.children[0].data
         // 全局执行，就有了 pageConfig.focusData 这个变量，保存着数组
-        global.eval('var pageConfig = {};');
-        global.eval(first);
+        global.eval('var pageConfig = {};')
+        global.eval(first)
         // 再去拿后面四个
-        let url = 'http://f.3.cn/index-floor?argv=focus&callback=jsonpCallbackFocus';
-        co(function *() {
-          try{
-            var res = yield req(url);
-            let ary = global.eval(res);
-            
-            let resultAry = [];
-            ary.data.forEach(item=> {
-              item.forEach(img=> {
-                resultAry.push(img.src);
-              })
+        let url = 'http://f.3.cn/index-floor?argv=focus&callback=jsonpCallbackFocus'
+        try{
+          var res = await req(url)
+          let ary = global.eval(res)
+          
+          let resultAry = []
+          ary.data.forEach(item=> {
+            item.forEach(img=> {
+              resultAry.push(img.src)
             })
+          })
 
-            pageConfig.focusData.forEach(item=> {
-              resultAry.push(item.src);
-            })
-            resolve(resultAry);
-          }catch(err) {
-            reject(err)
-          }
-        })
+          pageConfig.focusData.forEach(item=> {
+            resultAry.push(item.src)
+          })
+          resolve(resultAry)
+        }catch(err) {
+          reject(err)
+        }
       })
     }
   }
